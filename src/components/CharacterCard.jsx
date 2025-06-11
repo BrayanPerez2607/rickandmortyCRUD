73672dedab1e84de93955a3c75967ce1e04d0e42
Wirtz-46 <wilfergmart04@gmail.com>
2025-06-10 20:24:58 -0500

    eliminar

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,14 +1,15 @@
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, Button } from "react-native";
 import styles from "../styles/CharacterStyles";
 
 
-export default function CharacterCard ({character}) {
+export default function CharacterCard ({character, onDelete}) {
     return (
 
         <View style={styles.card}>
             <Image source={{uri: character.image}} style={styles.image}/>
             <Text style={styles.name}>{character.name}</Text>
-            <Text style={styles.info}>{character.status - character.species}</Text>
+            <Text style={styles.info}>{character.species}</Text> 
+            <Button size="small" title="Eliminar" color="red" onPress={onDelete}/>
         </View>
 
     );
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import { ActivityIndicator, ScrollView, Text, TouchableOpacity, Image, View, Button } from "react-native";
 import { fetchCharacters } from "../api/rickAndMortyApi";
 import styles from "../styles/CharacterStyles";
+import CharacterListScreen from "./CharacterListScreen";
 
-export default function HomeScreen () {
+export default function HomeScreen ({}) {
     const [characters, setCharacters] = useState([]);
     const [loading, setLoading] = useState(true);
     const loadCharacters = async () => {
@@ -29,6 +30,10 @@ export default function HomeScreen () {
         loadCharacters();
     }, [])
 
+    const deleteCharacter = (id) => {
+        setCharacters(characters.filter((c) => c.id !== id));
+    };
+
     return (
 
         <ScrollView contentContainerStyle={styles.container}>
@@ -40,7 +45,8 @@ export default function HomeScreen () {
                 <View style={{ flexDirection: 'row', gap: 10, marginBottom: 20, justifyContent: 'center' }}>
                     <Button title="Create" color={'blue'} />
                     <Button title="Edit" color={'blue'}/>
-                    <Button title="Delete" color={'blue'}/>
+                    <CharacterListScreen characters={characters} deleteCharacter={deleteCharacter}/>
+
                 </View>
             </View>
             }
